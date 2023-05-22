11211686f03b8f032f79ad10544cbf7759f4335e
Ricky Yang <cyang5075@gmail.com>
2023-05-22 20:09:32 +0800

    main

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -28,5 +28,10 @@ namespace ContosoPizza.Pages
             _service.AddPizza(NewPizza);
             return RedirectToAction("Get");
         }
+        public IActionResult OnPostDelete(int id)
+        {
+            _service.DeletePizza(id);
+            return RedirectToAction("get");
+        }
     }
 }
